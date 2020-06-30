0341fe61f4f73a5b204ce60919a0686614bc8bcf
Tarang Garlapally <taranggarlapally333@users.noreply.github.com>
2020-06-30 23:49:03 +0530

    firebase sdk included

--- src/login/Login.jsx
+++ src/login/Login.jsx
@@ -6,17 +6,17 @@ function Login() {
             <div className="col-md-6" style={{ paddingLeft: "4%" }}> <form><div class="form-group" style={{ width: "80%", marginTop: "40%" }} >
                 <input type="text" className="form-control" id="email" placeholder="Your Email" />
             </div>
-                <div class="form-group" style={{ width: "80%" }}>
+                <div className="form-group" style={{ width: "80%" }}>
                     <input type="password" className="form-control" id="password" placeholder="Your Password" />
-                </div><div class="form-group" style={{ width: "80%" }}>
-                    <input type="submit" className="form-control btn btn-warning" id="signin" value="Sign In" />
+                </div><div className="form-group submit" style={{ width: "80%" }}>
+                    <input type="submit" className="mybtn btn btn-warning" id="signin" value="Sign In" />
                 </div></form></div>
             <div className="col-md-6 social-login" > 
             <div className="sl-top">
                 
             </div>
-            <div className="sl-bottom"><input type="submit" className="sl btn btn-danger" id="google" value="Sign In With Google" />
-            <input type="submit" className="sl btn btn-primary" id="facebook" value="Sign In With Facebook" />
+            <div className="sl-bottom"><input type="submit" className=" mybtn sl btn btn-danger" id="google" value="Sign In With Google" />
+            <input type="submit" className="sl mybtn btn btn-primary" id="facebook" value="Sign In With Facebook" />
             </div>
             </div>
         </div>
